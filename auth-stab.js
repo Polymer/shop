88f4c91c41285a9a245ab79cf7590e5bd4ce93e3
Eiji Kitamura <agektmr@google.com>
2016-12-09 16:47:04 +0900

    Remove server code, add auth service worker

---
+++ auth-stab.js
@@ -0,0 +1,8 @@
+importScripts('/service-worker.js');
+
+// self.addEventListener('fetch', function(event) {
+//   event.respondWith(new Promise(function() {
+//     console.log('fetch');
+//     return fetch(event.request);
+//   }));
+// });
